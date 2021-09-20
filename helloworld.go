d495fb42be53023e3261ca4a288a049427746f7e
edsUlalan <edds.ulalan@gmail.com>
2021-09-20 22:33:30 +0800

    basic syntax and comment in go;

--- helloworld.go
+++ helloworld.go
@@ -2,5 +2,19 @@ package main
 import ("fmt")
 
 func main() {
-  fmt.Println("Hello World!")
+  fmt.Println("Hello World!") // fmt.Println() is a function made available from the fmt package
 }
+
+// A Go file consists of the following parts:
+// * Package declaration
+// * Import packages
+// * Functions
+// * Statements and expressions
+
+// You can write more compact code, like shown below
+// (this is not recommended because it makes the code more difficult to read)
+
+// package main; import ("fmt"); func main() { fmt.Println("Hello World!");}
+
+/* This is a multiline comment
+This is the second line */
