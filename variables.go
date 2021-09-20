6a40b3f477a74af5f2c6b78d450775e594b87dd8
edsUlalan <edds.ulalan@gmail.com>
2021-09-20 22:36:58 +0800

    variable declarations

---
+++ variables.go
@@ -0,0 +1,11 @@
+// var keyword
+
+var variablename type = value // You always have to specify either type or value (or both)
+
+// := sign
+
+variablename := value
+
+// In this case, the type of the variable is inferred from the value
+// (means that the compiler decides the type of the variable, based on the value)
+// It is not possible to declare a variable using :=, without assigning a value to it
