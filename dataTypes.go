be54f65c03664954e91f228268402ab7614026de
edsUlalan <edds.ulalan@gmail.com>
2021-09-24 07:06:28 +0800

    examples on data types

---
+++ dataTypes.go
@@ -0,0 +1,16 @@
+package main
+import ("fmt")
+
+func main() {
+  var a bool = true // Boolean
+  var b int = 5 // Integer
+  var c float32 = 3.14 // Floatiing point number
+  var d string = "Hi!" // string
+
+  fmt.Println("Boolean: ", a)
+  fmt.Println("Integer: ", b)
+  fmt.Println("Float: ", c)
+  fmt.Println("String: ", d)
+}
+
+// boolean data type
