22cead072f88f4295fb2e78469441c4f2c9657f0
edsUlalan <edds.ulalan@gmail.com>
2021-09-26 12:09:58 +0800

    complete examples on datatypes

--- dataTypes.go
+++ dataTypes.go
@@ -40,6 +40,16 @@ func signedInteger() {
   fmt.Printf("Type: %T, value: %v", y, y)
 }
 
+// int	Depends on platform:
+//     32 bits in 32 bit
+//     systems and 64 bit
+//     in 64 bit systems	- 2147483648 to 2147483647 in 32 bit systems and
+//                        - 9223372036854775808 to 9223372036854775807 in 64 bit systems
+// int8	  8 bits/1 byte	  - 128 to 127
+// int16	16 bits/2 byte	- 32768 to 32767
+// int32	32 bits/4 byte	- 2147483648 to 2147483647
+// int64	64 bits/8 byte	- 9223372036854775808 to 9223372036854775807
+
 // unsigned
 
 func unsignedInteger() {
@@ -47,4 +57,45 @@ func unsignedInteger() {
   var y uint = 4500
   fmt.Printf("Type: %T, value: %v", x, x)
   fmt.Printf("Type: %T, value: %v", y, y)
+}
+
+// uint	Depends on platform:
+//       32 bits in 32 bit
+//       systems and 64 bit
+//       in 64 bit systems	  0 to 4294967295 in 32 bit systems and
+//                           0 to 18446744073709551615 in 64 bit systems
+// uint8	  8 bits/1 byte	    0 to 255
+// uint16	16 bits/2 byte	  0 to 65535
+// uint32	32 bits/4 byte	  0 to 4294967295
+// uint64	64 bits/8 byte	  0 to 18446744073709551615
+
+// float data type
+
+func floatExample() {
+  var x float32 = 123.78
+  var y float32 = 3.4e+38
+  fmt.Printf("Type: %T, value: %v\n", x, x)
+  fmt.Printf("Type: %T, value: %v", y, y)
+}
+
+// float64 -  can store a larger set of numbers than float32
+
+func floatExampleTwo() {
+  var x float64 = 1.7e+308
+  fmt.Printf("Type: %T, value: %v", x, x)
+}
+
+// float32	32 bits	-3.4e+38 to 3.4e+38.
+// float64	64 bits	-1.7e+308 to +1.7e+308.
+
+// string data type
+
+func stringExample() {
+  var txt1 string = "Hello!"
+  var txt2 string
+  txt3 := "World 1"
+
+  fmt.Printf("Type: %T, value: %\n", txt1, txt1)  // Type: string, value: Hello!
+  fmt.Printf("Type: %T, value: %v\n", txt2, txt2) // Type: string, value:
+  fmt.Printf("Type: %T, value: %v\n", txt3, txt3) // Type: string, value: World 1
 }
