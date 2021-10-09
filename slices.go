0929e6568e9d757016b4f8e8037d9724ec2c2032
edsUlalan <edds.ulalan@gmail.com>
2021-10-10 05:16:00 +0800

    update slice array examples

--- slices.go
+++ slices.go
@@ -83,3 +83,73 @@ func exampleSliceFour() {
 // append elements to a slice
 // syntax
 slice_name = append(slice_name, element1, element2, ...)
+
+func exampleSliceFive() {
+  myslice1 := []int{1, 2, 3, 4, 5, 6}
+  fmt.Printf("myslice1 = %v\n", myslice1) // myslice1 = [1 2 3 4 5 6]
+  fmt.Printf("length = %d\n", len(myslice1)) // length = 6
+  fmt.Printf("capacity = %d\n", cap(myslice1)) // capacity = 6
+
+  myslice1 = append(myslice1, 20, 21)
+  fmt.Printf("myslice1 = %v\n", myslice1) // myslice1 = [1 2 3 4 5 6 20 21]
+  fmt.Printf("length = %d\n", len(myslice1)) // length = 8
+  fmt.Printf("capacity = %d\n", cap(myslice1)) // capacity = 12
+}
+
+// append one slice to another slice
+// syntax
+slice3 = append(slice1, slice2...)
+
+func exampleSliceSix() {
+  myslice1 := []int{1,2,3}
+  myslice2 := []int{4,5,6}
+  myslice3 := append(myslice1, myslice2...)
+
+  fmt.Printf("myslice3=%v\n", myslice3) // myslice3=[1 2 3 4 5 6]
+  fmt.Printf("length=%d\n", len(myslice3)) // length=6
+  fmt.Printf("capacity=%d\n", cap(myslice3)) // capacity=6
+}
+
+// change the length of a slice
+
+func exampleSliceSeven() {
+  arr1 := [6]int{9, 10, 11, 12, 13, 14} // An array
+  myslice1 := arr1[1:5] // Slice array
+  fmt.Printf("myslice1 = %v\n", myslice1) // myslice1 = [10 11 12 13]
+  fmt.Printf("length = %d\n", len(myslice1)) // length = 4
+  fmt.Printf("capacity = %d\n", cap(myslice1)) // capacity = 5
+
+  myslice1 = arr1[1:3] // Change length by re-slicing the array
+  fmt.Printf("myslice1 = %v\n", myslice1) // myslice1 = [10 11]
+  fmt.Printf("length = %d\n", len(myslice1)) // length = 2
+  fmt.Printf("capacity = %d\n", cap(myslice1)) // capacity = 5
+
+  myslice1 = append(myslice1, 20, 21, 22, 23) // Change length by appending items
+  fmt.Printf("myslice1 = %v\n", myslice1) // myslice1 = [10 11 20 21 22 23]
+  fmt.Printf("length = %d\n", len(myslice1)) // length = 6
+  fmt.Printf("capacity = %d\n", cap(myslice1)) // capacity = 10
+}
+
+
+// memory efficiency
+// The copy() function creates a new underlying array with only the required elements for the slice.
+// This will reduce the memory used for the program.
+// syntax
+copy(dest, src)
+
+func exampleSliceEight() {
+  numbers := []int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+  // Original slice
+  fmt.Printf("numbers = %v\n", numbers) // numbers = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15]
+  fmt.Printf("length = %d\n", len(numbers)) // length = 15
+  fmt.Printf("capacity = %d\n", cap(numbers)) // capacity = 15
+
+  // Create copy with only needed numbers
+  neededNumbers := numbers[:len(numbers)-10]
+  numbersCopy := make([]int, len(neededNumbers))
+  copy(numbersCopy, neededNumbers)
+
+  fmt.Printf("numbersCopy = %v\n", numbersCopy) // numbersCopy = [1 2 3 4 5]
+  fmt.Printf("length = %d\n", len(numbersCopy)) // length = 5
+  fmt.Printf("capacity = %d\n", cap(numbersCopy)) // capacity = 5
+}
