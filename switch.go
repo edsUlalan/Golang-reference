458ad48ee2046c9520c3429a72e5da37f11b0937
edsUlalan <edds.ulalan@gmail.com>
2021-10-15 02:21:06 +0800

    add examples on switch statements

---
+++ switch.go
@@ -0,0 +1,75 @@
+// Use the switch statement to select one of many code blocks to be executed.
+//
+// The switch statement in Go is similar to the ones in C, C++, Java, JavaScript, and PHP.
+// The difference is that it only runs the matched case so it does not need a break statement.
+
+// single switch
+
+package main
+import ("fmt")
+
+func main() {
+  day := 4
+
+  switch day {
+  case 1:
+    fmt.Println("Monday")
+  case 2:
+    fmt.Println("Tuesday")
+  case 3:
+    fmt.Println("Wednesday")
+  case 4:
+    fmt.Println("Thursday")
+  case 5:
+    fmt.Println("Friday")
+  case 6:
+    fmt.Println("Saturday")
+  case 7:
+    fmt.Println("Sunday")
+  }
+}  // Thursday
+
+// default keyword
+
+func defaultExample() {
+  day := 8
+
+  switch day {
+  case 1:
+    fmt.Println("Monday")
+  case 2:
+    fmt.Println("Tuesday")
+  case 3:
+    fmt.Println("Wednesday")
+  case 4:
+    fmt.Println("Thursday")
+  case 5:
+    fmt.Println("Friday")
+  case 6:
+    fmt.Println("Saturday")
+  case 7:
+    fmt.Println("Sunday")
+  default:
+    fmt.Println("Not a weekday")
+  }
+}  // Not a weekday
+
+// All the case values should have the same type as the switch expression.
+// Otherwise, the compiler will raise an error:
+
+// multi case switch statement
+
+func multiCaseExample() {
+  day := 5
+
+   switch day {
+   case 1,3,5:
+    fmt.Println("Odd weekday")
+   case 2,4:
+     fmt.Println("Even weekday")
+   case 6,7:
+    fmt.Println("Weekend")
+  default:
+    fmt.Println("Invalid day of day number")
+  }
+}  // Odd weekday
